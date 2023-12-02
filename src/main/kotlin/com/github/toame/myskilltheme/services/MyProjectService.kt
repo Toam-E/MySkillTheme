564f1a70669573bd28ad862e155854688cf391c2
GitHub Action <action@github.com>
2023-12-02 18:15:21 +0000

    Template cleanup

--- src/main/kotlin/com/github/toame/myskilltheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/toame/myskilltheme/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.toame.myskilltheme.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.toame.myskilltheme.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
